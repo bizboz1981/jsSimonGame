da1fd3bb3a7e3b65688c72f0e121d946dd54d65c
Bill <willjsaunders@icloud.com>
2024-04-07 08:35:57 +0100

    remove sample tests

--- assets/scripts/game.js
+++
@@ -1,8 +0,0 @@
-const sum = (a, b) => {
-    return a + b;
-};
-
-function clickAction() {
-    document.getElementById('par').innerHTML = 'you clicked me';
-};
-module.exports = { clickAction, sum };
